59f12697e92bbe148076554eadf4adbfc65dcb85
Sergey Bunas <Sergeibynas@gmail.com>
2018-11-04 14:06:15 +0300

    new feature

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta charset="utf8"/>
+    <title>Sick Fits!</title>
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css"></link>
+  </Head>
+);
+
+export default Meta;
---
+++ pages/sales.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sales = () => (
+  <div>
+    <p>sales</p>
+  </div>
+);
+
+export default Sales;
